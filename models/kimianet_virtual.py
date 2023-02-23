a828a8b12dfd8562b4af5604bf0594dc980aa1b2
Cindy Shi <xpshi@student.ubc.ca>
2023-02-22 20:06:33 -0800

    fixed bugs kimia: state_dict key loading discrepancy, created new state_dict under snapshots

--- models/kimianet_virtual.py
+++ models/kimianet_virtual.py
@@ -5,20 +5,20 @@ import torchvision
 import os
 
 class KimiaNet(nn.Module):
-    def __init__(self, num_classes, freeze=True, state_dict_path='/projects/ovcare/classification/cshi/OoD/vos/classification/CIFAR/snapshots/baseline/KimiaNetPyTorchWeights.pth'):
+    def __init__(self, num_classes=5, freeze=True, state_dict_path='/projects/ovcare/classification/cshi/OoD/vos/classification/CIFAR/snapshots/baseline/KimiaNetPyTorchWeights_new.pth'):
         '''
         load weights & customize final layer
         '''
         super(KimiaNet, self).__init__()
-        self.model = torchvision.models.densenet121()
+        self.model = torchvision.models.densenet121(num_classes=30)
         state_dict = torch.load(state_dict_path, map_location = torch.device('cpu'))
-        self.model.load_state_dict(new_state_dict)
+        self.model.load_state_dict(state_dict)
 
         num_ftrs = self.model.classifier.in_features
         self.model.classifier = torch.nn.Linear(num_ftrs, num_classes)
 
         nn.init.kaiming_normal_(self.model.classifier.weight.data, nonlinearity="relu")
-        self.model.classifier.bias.data = torch.zeros(size=self.model.fc.bias.size()).cuda()
+        self.model.classifier.bias.data = torch.zeros(size=self.model.classifier.bias.size()).cuda()
 
         if freeze:
             for name, param in self.model.named_parameters():
@@ -26,7 +26,7 @@ class KimiaNet(nn.Module):
                     param.requires_grad = False
     
     def forward(self, x):
-        print(type(x))
+        # print(type(x))
         features = self.model.features(x)
         out = F.relu(features, inplace=True)
         out = F.adaptive_avg_pool2d(out, (1, 1))
